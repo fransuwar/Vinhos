d5117449193f8d349be24f0f6e1e70d9b6e430bc
ufcnasescolas <ufcnasescolas@gmail.com>
2016-11-16 18:38:25 -0300

    bug fix #8, Confiração para servlet container externo.

--- src/main/java/br/com/fsp/wine/WineApplication.java
+++ src/main/java/br/com/fsp/wine/WineApplication.java
@@ -2,11 +2,20 @@ package br.com.fsp.wine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WineApplication {
+public class WineApplication extends SpringBootServletInitializer{
 
+	private static Class<WineApplication> applicationClass = WineApplication.class;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WineApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(applicationClass);
 	}
 }
